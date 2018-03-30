56dbaed3a938945033135148282f1d3df868ec83
BaykovaNadi <BaykovaNadi@yandex.ru>
2018-03-30 14:45:37 +0300

    commit 1

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-let dataCurrent = require('moment');
-moment().format();
-
+const dataCurrent = require('moment');
 let welcome = require('./welcome');
-var fio = document.getElementById("name");
-welcome(fio);
-
 let welclass = require('./welclass');
-var human = {firstName: "Vasya", lastName: "Pupkin", age: "25"};
-welclass (human);
 
 module.exports = {
     welcome, welclass
 };
+
+moment().format();
+
+let fio = document.getElementById("name");
+welcome(fio);
+
+let human = {firstName: "Vasya", lastName: "Pupkin", age: "25"};
+welclass (human);
